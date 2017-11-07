9d38111149fa5eec185f8251536300aeac0522ec
Dmitriy Makarov <redscoyote@gmail.com>
2017-11-07 23:38:25 +0300

    Homework #8

--- server.js
+++ server.js
@@ -20,25 +20,58 @@ app.get('/', function(req, res) {
 });
 
 app.get('/auto', function(req, res) {
-    res.render('pages/auto');
+    res.render('pages/auto', {auto: null});
 });
 
 app.post('/auto', function(req, res) {
-    var newId = 0;
-    auto.forEach(function(a) {
-        if(a.Id > newId)
-            newId = a.Id;
-    });
+    if (req.body.id && indexById(req.body.id) >= 0) {
+      const autoObj = auto[indexById(req.body.id)];
+      autoObj.Name = req.body.name;
+      autoObj.Year = parseInt(req.body.year);
+      autoObj.Price = parseInt(req.body.price);
+    } else {
+      var newId = 0;
+      auto.forEach(function (a) {
+        if (a.Id > newId)
+          newId = a.Id;
+      });
 
-    auto.push({
+      auto.push({
         Id: ++newId,
         Name: req.body.name,
         Year: req.body.year,
         Price: req.body.price
-    });
+      });
+    }
 
     res.redirect('/');
 });
+
+/** Модификация данных машины */
+app.get('/edit/:id', function(req, res) {
+  const index = indexById(req.params.id);
+  if (index >= 0) {
+    const auto2edit = auto[index];
+    res.render('pages/auto', {auto: auto2edit});
+  }
+});
+
+/** Удаление машины */
+app.get('/delete/:id', function(req, res) {
+  const index = indexById(req.params.id);
+  if (index >= 0) {
+      auto.splice(index, 1);
+  }
+  res.redirect('/');
+});
+
+/** Поиск индекса в массиве машин по Id машины */
+function indexById(id) {
+  let autoId = parseInt(id);
+  return auto.map(function (a) {
+    return a.Id === autoId;
+  }).indexOf(true);
+}
 
 app.listen(8080);
 console.log('8080 is the magic port');
